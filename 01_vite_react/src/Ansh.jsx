f33666a9ee52414c82232fdaeb3c6e8a87310e01
codewith-Ansh <anshh.ce@gmail.com>
2025-09-21 22:57:30 +0530

    create, use and rendering of components. (vite)

---
+++ 01_vite_react/src/Ansh.jsx
@@ -0,0 +1,9 @@
+function Ansh(){
+    return(
+        <h1>
+            The first file created in React!
+        </h1>
+    )
+}
+
+export default Ansh;
--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,9 +1,25 @@
+import { color } from "framer-motion"
+import Ansh from "./Ansh"
 
 function App() {
   return (
-    <h1>
-      My first React app with Vite!
-    </h1>
+    <>
+      <Ansh />
+      <h1>
+        Heading 1
+      </h1>
+      <h2>
+        Heading 2
+      </h2>
+      <h3>
+        Heading 3
+      </h3>
+      <p>
+        Returning multiple tags from a single function!
+      </p>
+    </>
+    
+    // <>...</> is called a Fragment. It allows us to return multiple elements from a function, since React components can only return one parent element. To handle multiple elements, we wrap them inside a Fragment.
   )
 }
 
