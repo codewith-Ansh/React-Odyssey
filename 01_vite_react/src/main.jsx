08a7702c38bf5b2f80ea054408fb5cacbe95d08b
codewith-Ansh <anshh.ce@gmail.com>
2025-09-23 22:47:48 +0530

    creation of react elements inside main.jsx

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,7 +1,44 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { h1 } from 'framer-motion/client'
+import { anticipate } from 'framer-motion'
+
+// function MyApp(){
+//     return (
+//         <>
+//             <h1>
+//                 Function inside main.jsx !
+//             </h1>
+//         </>
+//     )
+// }
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    "Click me to visit google"
+)
+// const anotherElement = (
+//     <a href="https://google.com" target='_blank'>Visit Google</a>
+// )
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    // <MyApp />
+    // MyApp()
+    reactElement
+    // anotherElement
+    // <App />
 )
