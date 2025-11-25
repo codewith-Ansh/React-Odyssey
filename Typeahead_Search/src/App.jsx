0aadb8e6553db367e484d411ee207c2e76794903
codewith-Ansh <anshh.ce@gmail.com>
2025-11-25 22:32:38 +0530

    Typeahead project - caching, loading states, API fetch

---
+++ Typeahead_Search/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import TypeAhead from "./components/TypeAhead";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Typehead search</h1>
+      <TypeAhead />
+    </div>
+  );
+}
