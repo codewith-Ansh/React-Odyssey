c1609a8879f5b1ba5895a72c2387c131ecfab2b3
codewith-Ansh <anshh.ce@gmail.com>
2025-12-01 21:35:31 +0530

    Update with changes

--- 10_ToDo_Context_Local/src/App.jsx
+++ 10_ToDo_Context_Local/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
 
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}   className='text-3xl font-bold underline'>
-          count is {count}
-        </button>
-
-        <h2  className='text-3xl font-bold underline'>Hello world</h2>
+    <TodoProvider value={{}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
       </div>
-    </>
+    </TodoProvider>
   )
 }
 
