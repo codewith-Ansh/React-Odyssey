14066c1b5f9f0bdd7ce8292ce829b529585369b0
codewith-Ansh <anshh.ce@gmail.com>
2025-12-10 11:34:26 +0530

    Update with new files

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from '../appwrite/auth'
 import './App.css'
 
 function App() {
-  const [content, setContent] = useState("Hello from Mega blog")
-  const changeContent = () => {
-    console.log(import.meta.env.VITE_APPWRITE_URL);
-    setContent("You clicked the heading!");
-  }
+  const [ loading, setLoading] = useState(true);
+  const [ posts, setPosts] = useState([]);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    
+  }, [])
 
   return (
     <>
-      <h1 class="text-3xl font-semibold underline text-center hover:text-blue-600 cursor-pointer mt-76" onClick={changeContent}>{content}</h1>
+      <h1 className="text-3xl font-semibold underline text-center hover:text-blue-600 cursor-pointer mt-76"></h1>
     </>
   )
 }
